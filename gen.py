1575f19b825baf5c15856472480eaed82b804810
M. Ajun Setiawan <71912420+setiaone-tech@users.noreply.github.com>
2023-02-13 21:07:27 +0700

    Update gen.py

--- gen.py
+++ gen.py
@@ -21,4 +21,5 @@ def index():
     return render_template("index.html",data=data, waktu=waktu)
     
 
-app.run(host='127.0.0.1', port='8080', debug=True)
+if '__name__' == '__main__':
+    app.run(host='127.0.0.1', port='8080', debug=True)
